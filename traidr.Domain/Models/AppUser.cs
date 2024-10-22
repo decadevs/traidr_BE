58f483c55656c9e74965818c530b51aaff3734c2
Fabian Chibuike Muoghalu <fabbenco97@gmail.com>
2024-10-22 09:28:32 +0100

    setup entityframework packages, added applicationdbcontext and created Indentityuser table (#1)

--- traidr.Domain/Models/AppUser.cs
+++ traidr.Domain/Models/AppUser.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using traidr.Domain.Enums;
 
 namespace traidr.Domain.Models
 {
-    public class AppUser
+    public class AppUser : IdentityUser
     {
+        public Gender? Gender { get; set; }
+        public int? Age { get; set; }
+        public string? ProfilePhoto { get; set; }
+        public string? ShopName { get; set; }
+        public bool IsSeller { get; set; } = false;
     }
 }
