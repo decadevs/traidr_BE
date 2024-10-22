b48aa7ec324cf7c6d1a25ae3d96223d27d3a2dae
GDBILLION <87760882+GDBILLION@users.noreply.github.com>
2024-10-22 15:11:50 +0100

    added Global exception handling (#2)

---
+++ traidr.Domain/ExceptionHandling/Exceptions/ConflictError409.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace traidr.Domain.ExceptionHandling.Exceptions
+{
+    public class ConflictError409 : Exception
+    {
+        public ConflictError409(string msg) : base(msg) { }
+    }
+}
